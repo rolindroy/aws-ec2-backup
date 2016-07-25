2a59ae7f1f6b53ceb15d27163b68033740772cab
Rolind Roy <rolind.roy@gmail.com>
2016-07-25 10:45:21 +0530

    Update ec2.sh

--- ec2.sh
+++ ec2.sh
@@ -31,13 +31,15 @@ exit 0
 instanceTag="Website"
 imageName="AMI_Website"
 
+echo -e "\nINFO:: Initializing Backup Script - Time                     :" $(date)
+echo -e "\nINFO:: Describing Ec2 Instance with Tag Name                 : $instanceTag !"
 
 instanceId=`aws ec2 describe-instances \
   --filters "Name=tag:Name, Values=$instanceTag" \
   --query 'Reservations[*].Instances[*].[InstanceId]' \
   --output text`
 
-echo "Selecting all AMIs that have the AMI name :" $imageName
+echo -e "\nINFO:: Selecting all AMIs that have the AMI name            :" $imageName
 
 desc=`aws ec2 describe-images --owner self \
   --filter Name=name,Values="$imageName" \
@@ -46,26 +48,30 @@ desc=`aws ec2 describe-images --owner self \
 
 echo ImageIds : $desc
 n=`echo "$desc" | wc -l`
-echo "# of images exist with the name of $imageName is " : $n
+echo -e "\nINFO:: # of images exist with the name of $imageName is     :" $n
 
 if [ ! -z $desc ];
 then
-    echo "Delete the expired images"
+    echo -e "\nWARN:: Delete the expired AMI                                :" $desc
     out=`aws ec2 deregister-image --image-id "$desc"`
+    echo -e "\nINFO:: Ami $desc delete status                               : "$out
     
     snapId=`aws ec2 describe-images --owner self \
         --filter Name=name,Values="$imageName" \
         --query 'Images[*].BlockDeviceMappings[*].Ebs[*].{ID:SnapshotId}' \
         --output text`
-    echo $snapId  
-    echo "Ami $desc delete status : "$out
+        
+    echo -e "\nWARN:: Delete Snapshot                                       :" $snapId
+    snapOut=`aws ec2 delete-snapshot --snapshot-id "$snapId"`
+    echo -e "\nINFO:: Snapshot $snapId delete status                        : "$out
 fi
 
-echo "creating new ami with the instance-id : "$instanceId
+echo -e "\nINFO:: Creating new ami with the instance-id                 : "$instanceId
+
 newAmi_id=`aws ec2 create-image \
   --no-reboot --instance-id "$instanceId" \
   --name "$imageName" --output text`
 
-echo $newAmi_id;
+echo -e "\nINFO:: New AMI Info                                           : "$instanceId
 
-echo "success !!"
+echo -e "\nSUCCESS!!"
