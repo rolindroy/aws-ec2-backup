e9b52569e27cdd32f9893cb9626196da71baf02e
Rolind Roy <rolind.roy@gmail.com>
2016-09-24 16:39:05 +0530

    Backup 7days (#1)
    * Update ec2.sh
    
    * Updated script to delete older snaapshots
    
    * Update ec2.sh
    
    * Update ec2.sh
    
    * Update ec2.sh
    
    * Update ec2.sh
    
    * Update ec2.sh
    
    * Update ec2.sh
    
    * Update ec2.sh
    
    * Update ec2.sh
    
    * Update ec2.sh
    
    * Update ec2.sh
    
    * Update ec2.sh
    
    * Update ec2.sh
    
    * Updated script to create multiple images
    
    * Updated script
    
    * Update
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * Modified script to delete older images
    
    * Fixed typo errors
    
    * Expire date modified
    
    * typo error fixed
    
    * Expiring condition changed

--- ec2.sh
+++ ec2.sh
@@ -1,79 +1,70 @@
-#!/bin/sh
-
-
-usage()
-{
-cat <<"USAGE"
-
-        Usage : ec2.sh [instance-tagName] [ami-name]
-          Note: [instance-tagName] => Tag Name of the instance.
-          Note: [ami-name] => The name of the AMI (provided during image creation)
-          
-        --
-        @author Rolind Roy <hello@rolindroy.com>
-          
-USAGE
-exit 0
-}
-
-## ---- use this option for multiple instances.----
-# if [ -z "$1" ];
-# then
-#   usage
-# fi
-
-# if [ -z "$2" ];
-# then
-#   usage
-# fi
-
-# instanceTag=$1
-# imageName=$2
-##----
-
+#!/bin/bash
+##Todo
 instanceTag="Website"
-imageName="AMI_Website"
+deletionDate=`date -u -d '-1day' +%Y%m%d`
+
+imageNamePrefix="AMI_"$instanceTag"_"
+currentDate=`date -u +%Y%m%d`
+imageName=$imageNamePrefix$currentDate
+
+
+echo $currentDate
+echo $deletionDate
 
 echo  "\nINFO:: Initializing Backup Script - Time  :" $(date)
-echo  "INFO:: Describing Ec2 Instance with Tag Name  : $instanceTag !"
+echo  "INFO:: Describing Ec2 Instance with Tag Name $instanceTag !"
 
 instanceId=`aws ec2 describe-instances \
   --filters "Name=tag:Name, Values=$instanceTag" \
   --query 'Reservations[*].Instances[*].[InstanceId]' \
   --output text`
 
-echo  "INFO:: Selecting all AMIs that have the AMI name  :" $imageName
+echo  "INFO:: Selecting all AMIs that have the AMI prefix " $imageNamePrefix
 
 desc=`aws ec2 describe-images --owner self \
-  --filter Name=name,Values="$imageName" \
-  --query 'Images[*].{ID:ImageId}' \
+  --filter Name=name,Values="$imageNamePrefix*" \
+  --query 'Images[*].Name' \
   --output text`
+  
+echo $desc
+n=`echo "$desc" | wc -w`
+echo  "INFO:: # of images exist with the prefix name of $imageNamePrefix is  :" $n
 
-n=`echo "$desc" | wc -l`
-echo  "INFO:: # of images exist with the name of $imageName is  :" $n
-
-if [ ! -z $desc ];
+if [ $n -ge "0" ] ;
 then
-    echo  "WARN:: Delete the expired AMI, AMI Id  :" $desc
-    out=`aws ec2 deregister-image --image-id "$desc"`
-    echo  "INFO:: Ami $desc delete status  : "$out
-    
-    snapId=`aws ec2 describe-images --owner self \
-        --filter Name=name,Values="$imageName" \
-        --query 'Images[*].BlockDeviceMappings[*].Ebs.{ID:SnapshotId}' \
-        --output text`
-        
-    echo  "WARN:: Delete Snapshot, Snapshot Id  :" $snapId
-    snapOut=`aws ec2 delete-snapshot --snapshot-id "$snapId"`
-    echo  "INFO:: Snapshot $snapId delete status  : "$out
+    for img in $desc
+    do
+		imagedate=`echo $img |  awk -F_ '{ print $3 }'`
+		if [ $deletionDate -ge $imagedate ]; 
+		then
+			expire_ami=`aws ec2 describe-images --owner self \
+			  --filter Name=name,Values="$img" \
+			  --query 'Images[*].{ID:ImageId}' \
+			  --output text`
+			
+			expire_snap=`aws ec2 describe-images --owner self \
+		   --filter Name=name,Values="$img" \
+		   --query 'Images[*].BlockDeviceMappings[*].Ebs.{ID:SnapshotId}' \
+		   --output text`
+		   
+		   echo  -e "\e[31;1m WARN:: Removing Expired AMI $expire_ami \e[0m"
+		   delete_ami=`aws ec2 deregister-image --image-id "$expire_ami"`
+		   echo  "INFO:: AMI $expire_ami removed. Status : $delete_ami"
+		  
+		   echo  -e "\e[31;1m WARN:: Removing Expired Snaphost $expire_snap \e[0m"
+		   delete_snap=`aws ec2 delete-snapshot --snapshot-id "$expire_snap"`
+		   echo  "INFO:: Snapshot $expire_snap removed. Status : $delete_snap"
+		   
+		fi
+    done
 fi
 
 echo  "INFO:: Creating new ami with the instance-id  : "$instanceId
 
 newAmi_id=`aws ec2 create-image \
-  --no-reboot --instance-id "$instanceId" \
-  --name "$imageName" --output text`
+ --no-reboot --instance-id "$instanceId" \
+ --name "$imageName" --output text`
 
-echo  "INFO:: New AMI Created, AMI Info  : "$instanceId
+echo  "INFO:: New AMI Created, AMI Info  : "$newAmi_id
 
 echo  "INFO:: Success !! $(date)\n"
